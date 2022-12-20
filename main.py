fc7439fc414973cf8804f635696cd1316b5cf1a9
Geo <george.nogovitsin@gmail.com>
2022-12-20 17:36:50 +0300

    small updates

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-import string
+from random import choice
+from string import ascii_letters
 
 
 def game():
@@ -9,26 +9,27 @@ def game():
              '4': ['display', 'It\'s a device.', 'Look at your display.'],
              '5': ['gallows', 'It\'s an execution "device".', 'And game.'],
              '6': ['horse', 'It\'s an animal.', 'It was very important animal ages ago.'],
-             '7': ['bicycle', 'It\'s an transport.', 'And sport device'],
+             '7': ['bicycle', 'It\'s an transport.', 'And sports equipment.'],
              '8': ['apple', 'It\'s a food.', 'And fruit'],
              '9': ['python', 'It\'s an animal.', 'And programming language...']}
     lives = 6
-    guess_word_index = random.choice(list(words))
+    guess_word_index = choice(list(words))
     guess_word = list(words[guess_word_index][0])
     playing_list = ['_' for _ in range(0, len(guess_word))]
     print(words[guess_word_index][1])
+    used_letters = []
 
     while lives > 0:
         print(playing_list)
         while True:
-            guess_letter = input('Guess the letter: ')
-            if guess_letter in list(string.ascii_letters):
+            guess_letter = input(f'Letters that you already used {used_letters}\nGuess the letter: ')
+            if guess_letter in list(ascii_letters):
                 guess_letter.lower()
                 break
             else:
                 print('Wrong value, you need to input only one letter from the English alphabet.')
         if guess_letter in guess_word:
-            print(f'Yes, it\'s correct! Letter {guess_letter} in that word.')
+            print(f'Yes, it\'s correct! Letter {guess_letter} in that word.\n')
             for index, letter in enumerate(guess_word):
                 if guess_letter == letter:
                     playing_list[index] = letter
@@ -39,10 +40,11 @@ def game():
                             game()
                         else:
                             quit()
-                    break
+
         else:
+            used_letters.append(guess_letter)
             lives -= 1
-            print(f'Didn\'t guess, haha :) \t{lives} tries remaining')
+            print(f'Didn\'t guess, haha :) \t{lives} tries remaining\n')
         if lives == 2:
             print(f'{words[guess_word_index][1]} {words[guess_word_index][2]} ')
         elif lives <= 0:
